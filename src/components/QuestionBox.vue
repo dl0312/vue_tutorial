d72c7d7344b0fd34e7e29c2ffe810e52141702cf
Geon Lee <leegun2003@gmail.com>
2019-07-02 18:29:50 +0900

    💅styling QuestionBox

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -3,7 +3,13 @@
     <b-jumbotron>
       <template slot="lead">{{ currentQuestion.question }}</template>
       <hr class="my-4" />
-      <p>List of answers</p>
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer,index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+        >{{ answer }}</b-list-group-item>
+      </b-list-group>
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button @click="next" variant="success" href="#">Next</b-button>
     </b-jumbotron>
@@ -14,6 +20,45 @@ export default {
   props: {
     currentQuestion: Object,
     next: Function
+  },
+  data() {
+    return {
+      selectedIndex: null
+    };
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+      console.log(index);
+    }
   }
 };
 </script>
+<style scoped>
+.btn {
+  margin: 0 0.5rem;
+}
+.list-group {
+  margin-bottom: 1rem;
+}
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
+}
+.selected {
+  background-color: blue;
+}
+.correct {
+  background-color: green;
+}
+.incorrect {
+  background-color: red;
+}
+</style>
