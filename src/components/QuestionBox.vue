99b96370317dda70a3e024b9c24ec81354ee4d6f
Geon Lee <leegun2003@gmail.com>
2019-07-02 16:10:13 +0900

    💅 styling with BootstrapVue

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
+      <template slot="lead">Some</template>
+
+      <hr class="my-4" />
+
+      <p>List of answers</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
